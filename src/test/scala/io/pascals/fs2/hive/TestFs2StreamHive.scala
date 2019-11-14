6fcce2f6cd92cbde0027aa70dc3471fe2784362d
pascals <advith.nagappa@gmail.com>
2019-11-14 16:42:35 +0100

    InvalidTable Exceptions

--- src/test/scala/io/pascals/fs2/hive/TestFs2StreamHive.scala
+++ src/test/scala/io/pascals/fs2/hive/TestFs2StreamHive.scala
@@ -1,16 +1,12 @@
 package io.pascals.fs2.hive
 
-import java.util
-
 import cats.effect.IO
 import fs2._
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.hive.conf.HiveConf
-import org.apache.hadoop.hive.metastore.api.NoSuchObjectException
 import org.apache.hive.streaming.{HiveStreamingConnection, StrictDelimitedInputWriter}
-import org.scalatest.{Assertion, FunSuite, Matchers}
+import org.scalatest.{FunSuite, Matchers}
 
-import scala.util.Try
 
 class TestFs2StreamHive extends FunSuite with Matchers {
 
@@ -20,11 +16,9 @@ class TestFs2StreamHive extends FunSuite with Matchers {
   val hiveConf = new HiveConf()
   hiveConf.addResource(new Path(HIVE_CONF_PATH))
 
-  val dbName = "test_db"
-  val tblName = "alerts"
-
   test("Simple Streaming Connection Test") {
-
+    val dbName = "test_db"
+    val tblName = "alerts"
     val writer: StrictDelimitedInputWriter = StrictDelimitedInputWriter.newBuilder()
       .withFieldDelimiter(',')
       .build()
@@ -37,20 +31,6 @@ class TestFs2StreamHive extends FunSuite with Matchers {
       .withRecordWriter(writer)
       .withHiveConf(hiveConf)
 
-
-    /*
-    val test: IO[Unit] = for {
-      connection <- IO(builder.connect())
-      _ <- IO(connection.beginTransaction())
-      _ <- IO(connection.write("13,value13,Asia,India".getBytes()))
-    } yield connection.commitTransaction()
-
-
-    test.unsafeRunSync()
-
-    */
-
-
     val f: Stream[IO, Unit] = Stream.bracket(IO.delay(builder.connect()))(conn => IO.delay(conn.close())).map{
       conn =>
       { conn.beginTransaction()
@@ -61,42 +41,39 @@ class TestFs2StreamHive extends FunSuite with Matchers {
 
     f.handleErrorWith{
      f => Stream.emit{
-       fail(s"Exception occurred. ${f.getCause}")
+         fail(s"Exception occurred. ${f}")
      }
     }.compile.drain.unsafeRunSync()
+  }
 
-    /*
-    val connection: HiveStreamingConnection = HiveStreamingConnection.newBuilder()
+  test("Invalid Table Streaming Connection Test") {
+    val dbName = "test"
+    val tblName = "alerts"
+    val writer: StrictDelimitedInputWriter = StrictDelimitedInputWriter.newBuilder()
+      .withFieldDelimiter(',')
+      .build()
+
+    val builder: HiveStreamingConnection.Builder = HiveStreamingConnection.newBuilder()
       .withDatabase(dbName)
       .withTable(tblName)
       .withAgentInfo("hive")
-      .withStreamingOptimizations(false)
+      .withStreamingOptimizations(true)
       .withRecordWriter(writer)
       .withHiveConf(hiveConf)
-      .connect()
 
-    // begin a transaction, write records and commit 1st transaction
-    try {
-      val a = connection.beginTransaction()
-      connection.write("11,value11,Asia,India".getBytes())
-      // connection.write("12,value12".getBytes())
-
-      connection.commitTransaction()
-      // begin another transaction, write more records and commit 2nd transaction
-      // connection.beginTransaction()
-      // connection.write("13,value13".getBytes())
-      // connection.write("14,value14".getBytes())
-      // connection.commitTransaction()
-      // close the streaming connection
-    }
-    catch {
-      case (e: Exception) => {
-        assertThrows(s"Exception occurred $e")
+    val f: Stream[IO, Unit] = Stream.bracket(IO.delay(builder.connect()))(conn => IO.delay(conn.close())).map{
+      conn =>
+      { conn.beginTransaction()
+        conn.write("17,value17,Africa,Nigeria".getBytes())
+        conn.commitTransaction()
       }
     }
-    finally {
-      connection.close()
-    }
-    */
+
+    f.handleErrorWith{
+      f => Stream.emit{
+        assert(f.isInstanceOf[org.apache.hive.streaming.InvalidTable])
+      }
+    }.compile.drain.unsafeRunSync()
   }
+
 }
