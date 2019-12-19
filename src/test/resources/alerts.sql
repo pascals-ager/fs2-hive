68e6b04228691b72165a26e6d98da3f4af602436
pascals <advith.nagappa@gmail.com>
2019-12-19 18:34:16 +0100

    Refactor test sql

--- src/test/resources/alerts.sql
+++ src/test/resources/alerts.sql
@@ -1,1 +1,17 @@
-create table alerts( id int , tracking_id string, msg string, continent string, country string, event_time timestamp) partitioned by (year int, month int, day int) clustered by (tracking_id) into 5 buckets stored as orc tblproperties("transactional"="true");
+create table `alerts`(
+     `id` int,
+     `tracking_id` string,
+     `msg` string,
+     `continent` string,
+     `country` string,
+     `event_time` timestamp)
+    partitioned by (
+        `year` int,
+        `month` int,
+        `day` int)
+    clustered by (
+        `tracking_id`)
+    into 5 buckets
+    stored as orc
+    tblproperties(
+        "transactional"="true");
--- src/test/resources/simple_alerts.sql
+++ src/test/resources/simple_alerts.sql
@@ -1,1 +1,10 @@
-create table simple_alerts ( id string , msg string ) partitioned by (continent string, country string) clustered by (id) into 5 buckets stored as orc tblproperties("transactional"="true");
+create table `simple_alerts` (
+    `id` string,
+    `msg` string )
+    partitioned by (
+        `continent` string,
+        `country` string)
+    clustered by (`id`)
+    into 5 buckets 
+    stored as orc 
+    tblproperties("transactional"="true");
